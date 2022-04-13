d32facf3ddc13ace2c8f5dbc3f0cc5fbfea54660
Enrique <enriquec@mit.edu>
2022-04-12 20:31:21 -0400

    Added smoothing to flashlight

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
-    public float moveSpeed;
+    public float smoothTime;
+    public float maxSpeed;
+
+    private Vector2 currentVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("IgnoredByFlashlight");
 
+        foreach (GameObject obj in objs)
+        {
+            Physics2D.IgnoreCollision(obj.GetComponent<Collider2D>(), this.GetComponent<Collider2D>());
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Adapted from https://gamedevbeginner.com/make-an-object-follow-the-mouse-in-unity-in-2d/
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 movementVector = Vector2.MoveTowards(transform.position, mousePosition, moveSpeed * Time.deltaTime);
+        Vector2 movementVector = Vector2.SmoothDamp(
+            transform.position,
+            mousePosition,
+            ref currentVelocity,
+            smoothTime,
+            maxSpeed
+        );
         Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
         rb.MovePosition(movementVector);
     }
