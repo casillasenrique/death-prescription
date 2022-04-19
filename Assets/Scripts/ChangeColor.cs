0bf407829bf5c274348d2d58ec49b12f6112ef03
Thanh Nguyen <thanhn1756@gmail.com>
2022-04-18 23:05:26 -0400

    calibration completed

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -1,28 +1,58 @@
 
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 public class ChangeColor : MonoBehaviour
 {
-    public static Color patientColor = new Color(128f,0f,11f);
-    public static Color doctorColor = new Color(0f,35f,128f);
+    public static Color patientColor;
+    public static Color doctorColor;
     [SerializeField] private Renderer myObject;
-    
-    public void ChangeMaterial (string player, string modifier)
-    {
-        float H, S, V;
 
+    private void Start()
+    {
+        patientColor = new Color(1f,0f,0.04f);
+        doctorColor = new Color(0,0.14f,1f);
+        // patientColor = new Color(1, 0, 0);
+        // doctorColor = new Color(0, 0, 1);
+
+
+    }
+    public void ChangeMaterial (string playerModifier)
+    {
+        string[] playerModifierArray  = playerModifier.Split(' ');
+        string player = playerModifierArray[0];
+        string modifier = playerModifierArray[1];
         Color colorToChange = player == "doctor"? doctorColor: patientColor;
-        Color.RGBToHSV(colorToChange, out H, out S, out V);
-        V += modifier == "brighter"? 1: -1;
-        V = Math.Max(19, V);
+
+        // if (modifier == "brighter")
+        // {
+        //     colorToChange.r += (255f-colorToChange.a)*.25f ;
+        //     colorToChange.g += (255f-colorToChange.b)*.25f ;
+        //     colorToChange.b += (255f-colorToChange.g)*.25f ;
+        // }
+        // else
+        // {
+        //     colorToChange.r *= .75f;
+        //     colorToChange.g *=.75f;
+        //     colorToChange.b *=.75f;
+        // }
+
+        Color filter = modifier == "brighter" ? new Color(1,1,1): new Color(0, 0, 0);
+        float alpha = .15f;
+        colorToChange.r += (filter.r - colorToChange.r) * alpha;
+        colorToChange.g += (filter.g - colorToChange.g) * alpha;
+        colorToChange.b += (filter.b - colorToChange.b) * alpha;
+        
+        
+        Debug.Log(colorToChange);
         if (player == "doctor")
         {
-            doctorColor = Color.HSVToRGB(H, S, V);
+            doctorColor = colorToChange;
             myObject.material.color = doctorColor;
         }
         else
         {
-            patientColor = Color.HSVToRGB(H, S, V);
+            patientColor = colorToChange;
             myObject.material.color = patientColor;
         }
 
