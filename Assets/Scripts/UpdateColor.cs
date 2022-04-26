2fd76f98ab9d36da0ad059043fafe57aee2a750a
Thanh Nguyen <thanhn1756@gmail.com>
2022-04-26 15:10:16 -0400

    updated colors changes for doctor and patient

--- Assets/Scripts/UpdateColor.cs
+++ Assets/Scripts/UpdateColor.cs
@@ -1,14 +1,17 @@
 
 using System;
 using UnityEngine;
+using Light2DE = UnityEngine.Experimental.Rendering.Universal.Light2D;
 public class UpdateColor : MonoBehaviour
 {
-    [SerializeField] private Renderer myObject;
+    // public GameObject myObject;
     [SerializeField] private string player;
     
     private void Start()
     {
-        myObject.material.color = player == "doctor"? ChangeColor.doctorColor: ChangeColor.patientColor;
+        var light = this.GetComponent<Light2DE>();
+        light.color = player == "doctor"? ChangeColor.doctorColor: ChangeColor.patientColor;
+        
     }
     
 }
