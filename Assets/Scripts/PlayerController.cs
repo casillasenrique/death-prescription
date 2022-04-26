f53229c3e909b209008bad3ebecc76d9886fbbe8
Enrique <enriquec@mit.edu>
2022-04-26 19:58:34 -0400

    Updated the doctor sprite

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,10 +11,13 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
 
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -28,7 +31,9 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal" + this.name);
         verticalInput = Input.GetAxisRaw("Vertical" + this.name);
         // Debug.Log(horizontalInput);
-        GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalInput, verticalInput).normalized * speed;
+        rb.velocity = new Vector2(horizontalInput, verticalInput).normalized * speed;
+
+
 
         // Animation
         animator.SetFloat("MovementX", horizontalInput);
@@ -36,6 +41,9 @@ public class PlayerController : MonoBehaviour
 
         if (horizontalInput != 0 || verticalInput != 0)
         {
+            // Only update the rotation if player is moving
+            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, rb.velocity) * Quaternion.Euler(0, 0, 90);
+            this.transform.rotation = toRotation;
             animator.SetFloat("lastMovementX", horizontalInput);
             animator.SetFloat("lastMovementY", verticalInput);
         }
