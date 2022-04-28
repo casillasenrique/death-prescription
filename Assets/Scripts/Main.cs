4deb1ed0cf0b5af75efc0ea5d974962c60d21a8b
m-kuoch <m.kuoch1@gmail.com>
2022-04-28 15:13:28 -0400

    update gen and exit spawn locations

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -11,38 +11,36 @@ public class Main : MonoBehaviour
     public GameObject generator;
     public Boolean generatorActive = false;
 
-    private List<int> xPos = new List<int>() {751, 184, 1159, 1532, 1774, 1163, 1456, 1772, 1772, 1399, 1301};
-    private List<int> yPos = new List<int>() {834, 924, 622, 622, 622, 404, 407, 407, 140, 140, 140}; 
+    private List<int> exXPos = new List<int>() {0, -560, 560, 0};
+    private List<int> exYPos = new List<int>() {-483, -30, -30, 464}; 
+    
+    private List<int> genXPos = new List<int>() {0, 0, -70, 330, 330, -380};
+    private List<int> genYPos = new List<int>() {-115, -310, 250, 235, -110, -95}; 
 
     // Start is called before the first frame update
     void Start()
     {
         // Generate 2 random numbers
         Random rnd = new Random();
-        int num = rnd.Next(0, xPos.Count);
-        int num2 = num;
-        int num3 = num;
-        int num4 = num;
-        while (num == num2)
+        int ex1 = rnd.Next(0, exXPos.Count);
+        int ex2 = ex1;
+        int gen1 = rnd.Next(0, genXPos.Count);
+        int gen2 = gen1;
+        while (ex1 == ex2)
         {
-            num2 = rnd.Next(0, xPos.Count);
+            ex2 = rnd.Next(0, exXPos.Count);
         }
         
-        while (num == num3 || num2 == num3)
+        while (gen1 == gen2)
         {
-            num3 = rnd.Next(0, xPos.Count);
+            gen2 = rnd.Next(0, genXPos.Count);
         }
-        
-        while (num == num4 || num2 == num4 || num3 == num4)
-        {
-            num4 = rnd.Next(0, xPos.Count);
-        }
-        
+
         // Add the exits to the game
-        Instantiate(exit, new Vector3(xPos[num], yPos[num], 0), Quaternion.identity);
-        Instantiate(exit, new Vector3(xPos[num2], yPos[num2], 0), Quaternion.identity);
-        Instantiate(generator, new Vector3(xPos[num3], yPos[num3], 0), Quaternion.identity);
-        Instantiate(generator, new Vector3(xPos[num4], yPos[num4], 0), Quaternion.identity);
+        Instantiate(exit, new Vector3(exXPos[ex1], exYPos[ex1], 0), Quaternion.identity);
+        Instantiate(exit, new Vector3(exXPos[ex2], exYPos[ex2], 0), Quaternion.identity);
+        Instantiate(generator, new Vector3(genXPos[gen1], genYPos[gen1], 0), Quaternion.identity);
+        Instantiate(generator, new Vector3(genXPos[gen2], genYPos[gen2], 0), Quaternion.identity);
         //DontDestroyOnLoad(this.gameObject);
     }
 
